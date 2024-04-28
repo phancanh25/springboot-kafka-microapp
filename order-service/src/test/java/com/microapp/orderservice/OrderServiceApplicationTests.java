934743e28e9d29642adee3fe0382e7bc79fdca8c
CanhPhan <toicanh25@gmail.com>
2024-04-28 16:16:46 +0700

    Done project

---
+++ order-service/src/test/java/com/microapp/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microapp.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
